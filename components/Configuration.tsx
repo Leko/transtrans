1a6d6b6c6dc154eb2adc8c1dbe96f2c7a6f10445
Leko <leko.noor@gmail.com>
2025-11-22 16:40:46 +0900

    fix: layout shift on lang select

--- components/Configuration.tsx
+++ components/Configuration.tsx
@@ -209,7 +209,7 @@ export default function Configuration({
             }
             disabled={isListening}
           >
-            <Select.Trigger className="flex items-center justify-between px-3 py-2 border border-gray-600 rounded-md bg-zinc-800 text-gray-100 disabled:opacity-50 disabled:cursor-not-allowed hover:bg-zinc-700 transition-colors">
+            <Select.Trigger className="h-10 flex items-center justify-between px-3 py-2 border border-gray-600 rounded-md bg-zinc-800 text-gray-100 disabled:opacity-50 disabled:cursor-not-allowed hover:bg-zinc-700 transition-colors">
               <Select.Value />
               <Select.Icon>
                 <ChevronDown className="w-4 h-4" />
@@ -246,7 +246,7 @@ export default function Configuration({
             }
             disabled={isListening}
           >
-            <Select.Trigger className="flex items-center justify-between px-3 py-2 border border-gray-600 rounded-md bg-zinc-800 ext-gray-100 disabled:opacity-50 disabled:cursor-not-allowed hover:bg-zinc-700 transition-colors">
+            <Select.Trigger className="h-10 flex items-center justify-between px-3 py-2 border border-gray-600 rounded-md bg-zinc-800 text-gray-100 disabled:opacity-50 disabled:cursor-not-allowed hover:bg-zinc-700 transition-colors">
               <Select.Value />
               <Select.Icon>
                 <ChevronDown className="w-4 h-4" />
